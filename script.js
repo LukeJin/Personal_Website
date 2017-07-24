2560c8c4fd4e26850f12a870e6db13472b4f3992
Luke Jin <luke.j857@gmail.com>
2017-07-24 02:33:35 -0400

    padding on body removed

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-
-
-(function (d,w){
+(function (d, w) {
 // Checks if page has loaded
-d.addEventListener("DOMContentLoaded", function(){
-	// adds an event listener to the button
-	d.getElementById("dropbtn").addEventListener("click", function (){
-		
-		d.getElementById("mydropdown").style.display = "block";
-		
-	})
-	
-	d.getElementById("submit_butt").addEventListener("click", function () {
-	
-		confirm('Are you ready to submit?');
-		
-	})
-	
-})
+    d.addEventListener("DOMContentLoaded", function () {
+        // adds an event listener to the button
+        d.getElementById("dropbtn").addEventListener("click", function () {
+
+            d.getElementById("mydropdown").style.display = "block";
+
+        })
+
+        d.getElementById("submit_butt").addEventListener("click", function () {
+
+            confirm('Are you ready to submit?');
+
+        })
+
+    })
 })(document, window);
